5339cd4663a0d53c68947041400cf23b032ac0b5
haru <haruki6180@gmail.com>
2022-03-23 14:10:47 +0900

    .storybook/main.jsを編集

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,17 @@ module.exports = {
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app"
   ],
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.(js|jsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        cacheDirectory: true,
+        presets: [require.resolve('@emotion/babel-preset-css-prop')],
+      },
+    });
+    return config;
+  },
   "framework": "@storybook/react",
   "core": {
     "builder": "webpack5"
