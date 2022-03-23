9a90cc5adb6a91d02bcf74f71c89248b450920a4
haru <haruki6180@gmail.com>
2022-03-23 13:23:59 +0900

    init

---
+++ src/components/circuler-menu/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const CirculerMenu = () => {
+
+  return (
+    <StyledWrapper>
+
+    </StyledWrapper>
+  );
+};
+
+const StyledWrapper = styled.div`
+display: flex;
+align-content: center;
+align-items: center;
+justify-content: center;
+min-height: 100vh;
+`
